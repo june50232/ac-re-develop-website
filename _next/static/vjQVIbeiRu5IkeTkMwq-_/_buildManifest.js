e5c98d5b0e8992ddefca25740d172ebd5d1c2e00
june50232 <s982535@gmail.com>
2022-09-04 23:45:47 +0000

    Deploying to gh-pages from @ june50232/ac-re-develop-website@d6cb53cbfd3cadf55ced45199b60ef71c56ced08 🚀

--- _next/static/vjQVIbeiRu5IkeTkMwq-_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7f1982e037266794.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/test":["static/chunks/pages/test-7e51908a75e5dda4.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
