3816e1228d082708d765e950f98cf59e34e2fc0c
June <s982535@gmail.com>
2023-01-04 12:47:53 +0800

    fix: image path parameter

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 const isProd = process.env.NODE_ENV === 'production';
-const awss3 = process.env.AWS_S3
 
 module.exports = {
   images: {
     loader: 'akamai',
-    path: awss3,
+    path: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
   },
   output: 'standalone',
   // assetPrefix: '/ac-re-develop-website',
   env: {
-    s3domain: awss3,
+    s3domain: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
   },
   basePath: isProd ? '/dev' : '',
 };
