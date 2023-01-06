f5b02ce1d0d849fa6d1f43550cd2bf9a4030c207
June <s982535@gmail.com>
2023-01-06 10:19:10 +0800

    fix: next config

--- next.config.js
+++ next.config.js
@@ -6,8 +6,9 @@ module.exports = {
   env: {
     s3Domain: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
     apiDomain: 'https://ac-re-api.onrender.com/',
+    // apiDomain: 'http://localhost:8080/',
   },
-  basePath: isProd ? '/dev' : '',
+  basePath: isProd ? '/ac-re-develop-website' : '',
   images: {
     loader: 'akamai',
     path: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
