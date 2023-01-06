1c7731c44fae7f7ab4372c3138cb22323f19d6e3
June <s982535@gmail.com>
2023-01-06 13:49:17 +0800

    fix: next config after change domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   output: 'standalone',
   // assetPrefix: '/ac-re-develop-website',
@@ -7,7 +5,7 @@ module.exports = {
     s3Domain: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
     apiDomain: 'https://ac-re-api.onrender.com/',
   },
-  basePath: isProd ? '/dev' : '',
+  basePath: '',
   images: {
     loader: 'akamai',
     path: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
