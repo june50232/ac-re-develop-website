6965ed67c30dd6e6f5752f35c8cf1ed82c463eaa
June <s982535@gmail.com>
2023-01-06 15:01:17 +0800

    feat: remove aws s3 setting

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   output: 'standalone',
   // assetPrefix: '/ac-re-develop-website',
   env: {
-    s3Domain: 'https://acre-website.s3.ap-northeast-1.amazonaws.com',
     apiDomain: 'https://ac-re-api.onrender.com/',
     // apiDomain: 'http://localhost:8080/',
   },
